4fe838349536eaa58477fb6db0edf40e2bca44bf
mirko <mirkoooo97@hotmail.it>
2019-01-12 11:38:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mirko/esercitazionebonus/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.mirko.esercitazionebonus;
+
+import android.content.Intent;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username, password;
+    Button accedi;
+    TextView errore;
+    TextView link;
+    public static final String PERSONA_EXTRA="com.example.mirko.esercitazionebonus.Persona";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        accedi = (Button)findViewById(R.id.accedi);
+        errore = (TextView) findViewById(R.id.errore);
+        errore.setVisibility(View.GONE); //di default la visibilità dell'error text è gone
+        link = (TextView) findViewById(R.id.link);
+        link.setText(Html.fromHtml("<u>Registrati ora!</u>"));
+        accedi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkInput()) {
+
+                    Intent showResults = new Intent(MainActivity.this,
+                            VediRisultati.class);
+                    Persona persona=new Persona();
+                    persona.setUsername(""+username.getText().toString());
+                    persona.setPassword(""+password.getText().toString());
+                    showResults.putExtra(PERSONA_EXTRA, persona);
+                    // richiama l'activity ShowResult
+                    startActivity(showResults);
+                }
+            }
+        });
+    }//CHIUDO ONCREATE
+
+
+    private boolean checkInput()
+    {
+        int errors = 0;
+
+        if(username.getText()==null || username.getText().length()==0) {
+            username.setError("Inserire username");
+            errors++;
+        }
+        else
+            username.setError(null);
+
+        if(password.getText()==null || password.getText().length()==0) {
+            password.setError("Inserire la password");
+            errors++;
+        }
+        else
+            password.setError(null);
+
+        switch (errors){
+            case 0:
+                errore.setVisibility(View.GONE);
+                errore.setText("");
+                break;
+            case 1:
+                errore.setVisibility(View.VISIBLE);
+                errore.setText("Si è verificato un Errore");
+                break;
+            default:
+                errore.setVisibility(View.VISIBLE);
+                errore.setText("Si sono verificati " + errors+" errori" );
+                break;
+        }
+        return errors==0; // ritorna true se non ci sono errori
+    }
+}
---
+++ app/src/main/java/com/example/mirko/esercitazionebonus/VediRisultati.java
@@ -0,0 +1,49 @@
+package com.example.mirko.esercitazionebonus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+
+public class VediRisultati extends AppCompatActivity {
+
+    Persona p1;
+    Persona p2;
+    TextView r;
+    Button indietro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vedi_risultati);
+
+        r = (TextView) findViewById(R.id.result);
+        indietro = (Button) findViewById(R.id.indietro);
+
+        Intent intent = getIntent();
+        Serializable obj = intent.getSerializableExtra(MainActivity.PERSONA_EXTRA);
+        Bundle bundle = getIntent().getExtras();
+
+        p1 = (Persona)obj;
+        r = (TextView)findViewById(R.id.result);
+        p2 = PersonaFactory.login(p1.getUsername(),p1.getPassword());
+        if(p2 != null)
+            r.setText("Benvenuto "+p2.getUsername());
+        else
+            r.setText("Accesso Negato!");
+
+        indietro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent undo = new Intent(VediRisultati.this, MainActivity.class);
+                startActivity(undo);
+            }
+        });
+    }
+}
